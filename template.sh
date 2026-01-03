912abf6e454204e402677d71ee9c007b0effc8e9
Abhishek Kori <abhishekkori601@gmail.com>
2026-01-03 16:59:26 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,22 @@
+mkdir -p backend
+mkdir -p services
+mkdir -p utils
+mkdir -p vapi
+mkdir -p scripts
+
+touch backend/main.py
+touch backend/config.py
+touch backend/schemas.py
+
+touch backend/services/calendar_service.py
+touch backend/services/oauth_service.py
+
+touch backend/utils/logger.py
+
+touch vapi/agent_prompt.txt
+
+touch scripts/create_event_test.py
+
+touch requirements.txt
+touch .env.example
+
